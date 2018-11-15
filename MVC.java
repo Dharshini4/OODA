67a8a0469b90a29c1a07c036a3e0698e9e2f1bcb
Dharshini4 <jjdharshini4@gmail.com>
2018-11-14 22:31:01 -0700

    Add files via upload

--- MVC.java
+++ MVC.java
@@ -1,42 +1,107 @@
 package pos.controller;
-
-import pos.model.PizzaModel;
-import pos.view.PizzaView;
-
-import pos.model.OrderModel;
-import pos.view.OrderView;
-
+import pos.template.CheesePizza;
+import pos.model.*;
+import pos.view.*;
+import pos.template.*;
 
 
 public class MVC {
 	   public static void main(String[] args) {
 
-		      //fetch student record based on his roll no from the database
+		   // Using template design pattern to create Cheese Pizza
+		   
+		   PizzaTemplate cheesepizza= new CheesePizza();
+		   cheesepizza.makePizza("yes","large","thin","Mozarella");
+		   System.out.println("Cheese Pizza is ready!");
+		   System.out.println("-----------------------");
+		   PizzaTemplate pepperonipizza= new PepperoniPizza();
+		   pepperonipizza.makePizza("no","med","thick","Cheddar");
+		   System.out.println("Pepperoni Pizza is ready!");
+		   System.out.println("-----------------------");
+		   
+		  /* 
+		   //fetch data from the database, Checking Pizza MVC
 		      PizzaModel model  = retrivePizzaFromDatabase();
-
-		      //Create a view : to write student details on console
 		      PizzaView view = new PizzaView();
-
 		      PizzaController controller = new PizzaController(model, view);
-
 		      controller.updateView();
-
-		      //update model data
-		      controller.setposmodelSize("large");
-
+		      //Update model data
+		      //controller.setposmodelSize("large");
 		      controller.updateView();
 		      
+		   //fetch data from the database, Checking Order MVC
 		      OrderModel o_model  = retriveOrderFromDatabase();
 		      OrderView o_view = new OrderView();
 		      OrderController o_controller = new OrderController(o_model, o_view);
 		      o_controller.updateView();
 		      o_controller.OrderModelOrderQuantity(2);
 		      o_controller.updateView();
+		      */
+		      //fetch data from the database, Checking Topping MVC
+		      ToppingModel t_model  = retriveToppingFromDatabase();
+		      ToppingView t_view = new ToppingView();
+		      ToppingController t_controller = new ToppingController(t_model, t_view);
+		      t_controller.updateView();
+		      t_controller.ToppingModelToppingName("Olives");
+		      t_controller.updateView();
+		      
+		    //fetch data from the database, Checking User MVC
+		      UserModel u_model  = retriveUserFromDatabase();
+		      UserView u_view = new UserView();
+		      UserController u_controller = new UserController(u_model, u_view);
+		      u_controller.updateView();
+		      u_controller.UserModelUserMobileNumber("7208942137");
+		      u_controller.updateView();
+		      
+		    //fetch data from the database, Checking Payment MVC
+		      PaymentModel p_model  = retrivePaymentFromDatabase();
+		      PaymentView p_view = new PaymentView();
+		      PaymentController p_controller = new PaymentController(p_model, p_view);
+		      p_controller.updateView();
+		      //p_controller.PaymentModelPaymentAmount(720);
+		      //p_controller.updateView();
+		      
+		    //fetch data from the database, Checking Menu MVC
+		      MenuModel m_model  = retriveMenuFromDatabase();
+		      MenuView m_view = new MenuView();
+		      MenuController m_controller = new MenuController(m_model, m_view);
+		      m_controller.updateView();
+		      m_controller.MenuModelItemPrice(15);
+		      m_controller.updateView();
+		    
 		      
 		      
+		      		      
 		   }
-
-		   private static PizzaModel retrivePizzaFromDatabase(){
+		   private static ToppingModel retriveToppingFromDatabase(){
+		      ToppingModel topping=new ToppingModel();
+			  topping.setToppingName("Onion");
+			  topping.setToppingQuantity(2);
+			  return topping;
+		   }
+		   
+		   private static UserModel retriveUserFromDatabase(){
+			      UserModel user=new Customer();
+				  user.setUserName("Amy");
+				  user.setUserMobileNumber("7208942134");
+				  user.setUserAddress("124, XYZ St, Boulder, CO");
+				  return user;
+			   }
+		   
+		   private static MenuModel retriveMenuFromDatabase(){
+			   	MenuModel menu=new MenuModel();
+				menu.setItemId(12);
+				menu.setItemPrice(12);
+				menu.setItemName("Veggie Delight");
+			   	return menu;
+			   }
+		   private static PaymentModel retrivePaymentFromDatabase(){
+			   PaymentModel payment=new PayPal();
+			   payment.setPaymentId(110);
+			   payment.makePayment(1002435,123);
+				  return payment;
+			   }
+	/*	   private static PizzaModel retrivePizzaFromDatabase(){
 		      PizzaModel pizza=new PizzaModel();
 			  pizza.setCheese("low");
 			  pizza.setVegan("yes");
@@ -53,4 +118,4 @@ public class MVC {
 				  order.setTotalAmount(100);
 				  return order;
 		   }
-}
+*/}
--- PaymentController.java
+++ PaymentController.java
@@ -1,5 +1,32 @@
 package pos.controller;
+import pos.model.PaymentModel;
+import pos.view.PaymentView;
 
 public class PaymentController {
+	private PaymentModel model;
+	private PaymentView view;
+
+	public PaymentController(PaymentModel model, PaymentView view){
+	      this.model = model;
+	      this.view = view;
+	   }
+	public void PaymentModelPaymentId(int paymentId){
+	      model.setPaymentId(paymentId);		
+	   }
+
+	public int getPaymentId(){
+	      return model.getPaymentId();		
+	   }
+	public void PaymentModelPaymentAmount(int paymentNumber, int code){
+	      model.makePayment(paymentNumber, code);		
+	   }
+
+	public int getPaymentDue(){
+	      return model.getPaymentDue();		
+	   }
+	
+	public void updateView(){				
+	      view.printPaymentDetails(model.getPaymentId(),model.getPaymentDue());
+	   }
 
 }
