f2e95f3204131d777b57f0484412157b25c6560c
Dharshini4 <jjdharshini4@gmail.com>
2018-11-28 16:42:04 -0700

    Delete ToppingView.java

--- ToppingView.java
+++
@@ -1,9 +0,0 @@
-package pos.view;
-
-public class ToppingView {
-	public void printToppingDetails(String toppingName, int toppingQuantity ){
-	      System.out.println("Topping Specifications: ");
-	      System.out.println("Topping name: "+toppingName);
-	      System.out.println("Topping quantity: "+toppingQuantity);
-}
-}
