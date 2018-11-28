f48fd02fc6a6f5f84d52c40ae977c03f7dd324f1
Dharshini4 <jjdharshini4@gmail.com>
2018-11-28 16:44:33 -0700

    Delete PaymentView.java

--- PaymentView.java
+++
@@ -1,9 +0,0 @@
-package pos.view;
-
-public class PaymentView {
-	public void printPaymentDetails(int paymentId, int paymentAmount ){
-	      System.out.println("Payment Details: ");
-	      System.out.println("Payment Id: "+paymentId);
-	      System.out.println("Amout paid: "+paymentAmount+ "$");
-	    	}
-}
