4a9a68a41cd64945730e16f59e0e2eb75e38b69f
Dharshini4 <jjdharshini4@gmail.com>
2018-11-28 16:30:09 -0700

    Rename package-search-index.js to Jaiganesh_PizzaPointOfSale_Part6Code/package-search-index.js

---
+++ Jaiganesh_PizzaPointOfSale_Part6Code/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"pos.builder"},{"l":"pos.controller"},{"l":"pos.decorater"},{"l":"pos.factoryandtemplate"},{"l":"pos.model"},{"l":"pos.view"}]
--- package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"pos.builder"},{"l":"pos.controller"},{"l":"pos.decorater"},{"l":"pos.factoryandtemplate"},{"l":"pos.model"},{"l":"pos.view"}]
